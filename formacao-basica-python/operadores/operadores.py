5793ea05ef8164cbf7119d55727c41d98443c38c
Anderson Abreu Rabelo <andersonabreurabelo.9@gmail.com>
2025-02-02 18:38:37 -0400

    Estudo PYTHON

---
+++ formacao-basica-python/operadores/operadores.py
@@ -0,0 +1,27 @@
+#Operadores: Aritméticos, relacional, Lógico e de associação. 
+
+#Aritméticos:
+
+1+2
+2+3
+lista= ['Anderson'+'Adejair'+'Patricia'+'Neide']
+print(lista)
+print(len(lista))
+
+lua = 'gata' * 20
+print(lua)
+
+estrela = 2 ** 4 #expomemciação
+print(estrela)
+
+#divisão simples
+marte = 10/5
+print(marte)
+
+#Divisão inteira
+marte1 = 10 // 5
+print(marte1)
+
+# Módulo operador de módulo, que retorna o resto da divisão entre dois números.
+teste = 10 % 3
+print(teste)
