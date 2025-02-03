41ca41ef1a3d052f74e4963c0fd1993ad2ebb829
Anderson Abreu Rabelo <andersonabreurabelo.9@gmail.com>
2025-02-03 18:33:04 -0400

    Exercios Python

---
+++ Exercicios/indentação Python.py
@@ -0,0 +1,17 @@
+#######Python Sintaxe#######
+
+#Observe que se não houver indentação, não irá funcionar a copilação.
+#  O número de espaços depende de você como programador, o uso mais comum é quatro, mas tem ser pelo menos um.
+
+#if 77 > 7:
+#print("É maior!")
+#else:
+#print("É menor")
+
+#Forma correta:
+
+if 6 > 7:
+   print('É maior!')
+else:
+   print('É menor!')
+
