b904e6134e205309cc30dca4cb4b42c378da722b
Anderson Abreu Rabelo <andersonabreurabelo.9@gmail.com>
2025-02-23 20:14:34 -0400

    Exercicios Python

---
+++ Exercicios/Exercicios Datas Types e Strings.py
@@ -0,0 +1,27 @@
+"""1. Data Types
+Exercício 1: Crie uma variável chamada numero e atribua o valor 5. Em seguida, crie outra variável chamada texto e atribua o valor "Olá, Mundo!". Exiba no console os tipos dessas variáveis.
+Exercício 2: Crie uma variável chamada flutuante e atribua o valor 3.14. Verifique e imprima se o tipo dessa variável é float.
+
+2. Casting
+Exercício 3: Crie uma variável chamada inteiro com valor 10 e uma variável chamada flutuante com valor 3.5. Faça o cast de flutuante para inteiro e exiba o resultado no console.
+Exercício 4: Receba um valor numérico em formato string (por exemplo, "123") e converta para o tipo inteiro. Em seguida, multiplique esse número por 2 e exiba o resultado.
+
+3. Strings
+Exercício 5: Crie uma variável chamada nome e atribua o seu nome como valor. Depois, crie uma outra variável chamada sobrenome e atribua o seu sobrenome. Concatene essas duas variáveis em uma única string e exiba no console.
+Exercício 6: Crie uma string qualquer e use a função len() para calcular o tamanho da string. Exiba o resultado.
+
+4. Slicing Strings
+Exercício 7: Crie uma string com o valor "Python é incrível!". Exiba apenas a palavra "Python" utilizando slicing.
+Exercício 8: Dada a string "Aprendendo Python", extraia e exiba os três primeiros caracteres da string.
+
+5. Modifying Strings
+Exercício 9: Crie uma variável frase com o valor "Eu adoro Python!". Substitua a palavra "Python" por "programação" e exiba o resultado.
+Exercício 10: Crie uma string com o valor "Este é um exemplo". Converta toda a string para letras maiúsculas e exiba o resultado.
+
+6. Escape Characters
+Exercício 11: Crie uma string que exiba a frase: O "Python" é uma linguagem divertida! utilizando escape characters.
+Exercício 12: Crie uma string que contenha uma nova linha e um tabulador. Exiba a string no console.
+7. String Methods
+Exercício 13: Crie uma string com o valor " python ". Use o método strip() para remover os espaços em branco do início e do final da string, e exiba o resultado.
+Exercício 14: Crie uma variável chamada frase com o valor "Eu gosto de aprender Python". Use o método replace() para substituir a palavra "aprender" por "ensinar" e exiba a nova frase.
+Exercício 15: Crie uma variável texto com o valor "aprendendo Python". Use o método upper() para transformar todas as letras em maiúsculas e exiba a string resultante."""
