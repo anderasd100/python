42503342de260ff4bb63685141e32cbae558b1f1
Anderson Abreu Rabelo <andersonabreurabelo.9@gmail.com>
2025-03-07 19:56:09 -0400

    Revisão tópicos PY

---
+++ Arvore PY.py
@@ -0,0 +1,43 @@
+#--------Systax----------#
+py
+#--------Indentation----------#
+#--------Comment----------#
+#
+'' Single
+"" Double
+""" """ Triple
+#--------Variables----------#
+-casting: specify the data type 
+- type
+-Case Sentitive
+- Many Values to multiple variables , , ,
+- One value to multiple variables  = = =
+- Global Variables #Função / global x
+
+#--------Data Types----------#
+#--------Numbers----------#
+#--------Casting----------#
+#--------Strings----------#
+-Strings are Arrays
+-Looping Through a String # For x in "python":
+-Function Len()
+-Check String in 
+-Slicing Strings []
+-Upper Case
+-Lower case
+-Remove Whitespace #strip()
+-Replace String
+-Split String
+-Concatenation
+-F-Strings
+-Escape Character # txt = "We are the so-called \"Vikings\" from the north."
+
+#-------Booleans-----------#
+#-------Operators-----------#
+-Arithmetic Operators
+-Assignment Operators
+-Comparison Operators
+-Logical Operators
+
+#-------Lists-----------#
+
